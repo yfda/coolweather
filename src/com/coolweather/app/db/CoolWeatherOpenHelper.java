1a626b10e6275d522042760481aa929c677e4c13
yfda <wyf1988@live.com>
2015-05-25 15:51:39 +0800

    coolWeather

---
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,46 @@
+package com.coolweather.app.db;
+
+import android.R.integer;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+	public static final String CREATE_PROVINCE = "create table Province ("
+			+ "id integer primary key autoincrement, " + "province_name text, "
+			+ "province_code text)";
+	/**
+	 * City表建表语句
+	 */
+	public static final String CREATE_CITY = "create table City ("
+			+ "id integer primary key autoincrement, " + "city_name text, "
+			+ "city_code text, " + "province_id integer)";
+	/**
+	 * County表建表语句
+	 */
+	public static final String CREATE_COUNTY = "create table County ("
+			+ "id integer primary key autoincrement, " + "county_name text, "
+			+ "county_code text, " + "city_id integer)";
+
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO 自动生成的方法存根
+		db.execSQL(CREATE_COUNTY);
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
